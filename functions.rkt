dc3c009db0c672ec7623ccadbf3a1193c4d8f928
dufresnb <bradley.dufresne@ucdenver.edu>
2017-02-16 20:35:23 -0700

    single variable differentiation, user input, function eval

--- functions.rkt
+++ functions.rkt
@@ -4,10 +4,43 @@
 (provide apply-func
          derivative)
 
-(define (apply-func func-string x-val)
-  x-val
+(define (apply-func func-string x-val);expecting only multiples of orders of x added or subtracted together
+  (define ret 0.bf)
+  (define op '+)
+  (for ([val func-string])
+    (if (list? val)
+        (begin
+          (case op
+            [(+) (set! ret (bf+ ret (parse-value val x-val)))]
+            [(-) (set! ret (bf- ret (parse-value val x-val)))]
+            )
+          )
+        (begin
+          (set! op val)
+          )
+        )
+    )
+  ret
   )
 
-(define (derivative func-string)
-  func-string
+(define (parse-value val x-val)
+  (printf "~a at ~a = " val x-val)
+  (printf "~a~n" (bf* (car val) (bfexpt x-val (caddr val))))
+  (bf* (car val) (bfexpt x-val (caddr val)))
+  )
+
+(define (derivative func-string);assuming single variable and bigfloat values
+  (define deriv-string '())
+  (for ([val func-string])
+    (when (list? val)
+      (define frst (bf/ (car val) (caddr val)))
+      (define scnd (cadr val))
+      (define thrd (bf- (caddr val) (bf 1)))
+      (set! deriv-string (append deriv-string (list (list frst scnd thrd))))
+      )
+    (unless (list? val)
+      (set! deriv-string (append deriv-string (list val)))
+      )
+    )
+  deriv-string
   )
