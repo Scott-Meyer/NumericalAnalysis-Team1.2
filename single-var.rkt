86161cb9dab5350fe3c7159f38f40425ddf0499b
Scott Meyer <Scott.R.Meyer@gmail.com>
2017-03-02 19:32:26 -0700

    Make bisection in single-var.rkt work like newtons-method

--- single-var.rkt
+++ single-var.rkt
@@ -1,14 +1,21 @@
 #lang racket
 (require math/bigfloat
-         "functions.rkt")
+         "functions.rkt"
+         "Bisection.rkt")
 
 (provide bisection
          fixed-point
          newtons-method)
 
 ;Scott
+;example:
+;f(x)=3x^3-1, (-2, 2), 8 iterations
+;(bisection 8 (list -2 2) (list (list (bf 3) 'x (bf 3)) '- (list (bf 1) 'x 0.bf)))
 (define (bisection num-iterations initial-guess input-string)
-  void
+  (define in (list (bf (first initial-guess)) (bf (second initial-guess))))
+  (define (fx x)
+    (apply-func input-string x))
+  (bisectionProcess num-iterations fx in)
   )
 
 ;Scott
